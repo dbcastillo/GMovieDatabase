1a75a7e501beb749b32f4c245a4ef7212e2666d8
Castillo <905903@cognizant.com>
2021-06-05 09:58:08 -0500

    Initial commit

---
+++ src/main/java/com/example/gmoviedatabase/GMovieDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.gmoviedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GMovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GMovieDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gmoviedatabase/GMovieDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmoviedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GMovieDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
