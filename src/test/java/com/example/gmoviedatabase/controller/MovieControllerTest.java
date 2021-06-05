6924934db308306695ecbc646c7dc9c2d0a0a0c0
sininoble <sininoble@gmail.com>
2021-06-05 13:03:13 -0400

    Added post movie method and impl

--- src/test/java/com/example/gmoviedatabase/controller/MovieControllerTest.java
+++ src/test/java/com/example/gmoviedatabase/controller/MovieControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,6 +23,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 
 import javax.transaction.Transactional;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -41,24 +45,26 @@ public class MovieControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON);
         this.mvc.perform(request)
-                .andExpect(status().isOk());
-                //.andExpect(jsonPath("$[0].id", is("")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").doesNotExist());
+
     }
 
-//    @Test
-//    @Transactional
-//    @Rollback
-//    public void testCreateMovie() throws Exception {
-//        Movie movie = new Movie();
-//        movie.setTitle("The Avengers");
-//        this.repository.save(movie);
-//        MockHttpServletRequestBuilder request=post("/movies")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON);
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[0].title", is("The Avengers")));
-//    }
-
+    @Test
+    @Transactional
+    @Rollback
+    public void testCreateMovie() throws Exception {
+        String json=getJSON("src/test/resources/postMovie.json");
+        MockHttpServletRequestBuilder request=post("/movies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("The Avengers")));
+    }
+    public String getJSON(String path) throws  Exception{
+        Path paths = Paths.get(path);
+        return new String(Files.readAllBytes(paths));
+    }
 
 }
